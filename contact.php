dcf5df765d833cb64c115c11f2d0895dd1a31ac7
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-21 17:34:43 +0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>This Is My About Website</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Contact Me</p>";
+        ?>
+        <a href="https://t.me/kurayana"> Click here to login 
+        <a href="https://www.instagram.com/neekusoo/"> Click here to register 
+    </body>
+</html> 
